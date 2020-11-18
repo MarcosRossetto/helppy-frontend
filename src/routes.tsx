4864916783917e3b867508e722189bc4c135865e
Marcos Rossetto <marcosrst23@gmail.com>
2020-11-18 18:33:26 -0300

    Testando Rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Landing from './pages/Landing'
 import CallForm from './pages/CallForm'
@@ -8,9 +8,11 @@ import Login from './pages/Login'
 function Routes(): ReactElement {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Landing} />
-      <Route path="/call-form" exact component={CallForm} />
-      <Route path="/login" exact component={Login} />
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/call-form" exact component={CallForm} />
+        <Route path="/login" exact component={Login} />
+      </Switch>
     </BrowserRouter>
   )
 }
