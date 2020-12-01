19e8ababbf3c7b72a1659a20c4e18f7450ec4671
Marcos Rossetto <marcosrst23@gmail.com>
2020-12-01 20:00:09 -0300

    bug fix

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -4,21 +4,17 @@ import './styles.css'
 
 function NotFound(): ReactElement {
   return (
-    <div>
-      <div id="clouds">
-        <div className="cloud x1" />
-        <div className="cloud x1_5" />
-        <div className="cloud x2" />
-        <div className="cloud x3" />
-        <div className="cloud x4" />
-        <div className="cloud x5" />
-      </div>
-      <div className='c'>
-        <div className='_404'>404</div>
-        <hr />
-        <div className='_1'>THE PAGE</div>
-        <div className='_2'>WAS NOT FOUND</div>
-        <a className='btn' href='/'>BACK TO HOME</a>
+    <div className="content-not-found">
+      <div id="title">Simple Pure CSS3 &bull; 404 Error Page</div>
+      <div className="circles">
+        <p>
+          404
+          <br />
+          <small>PAGE NOT FOUND</small>
+        </p>
+        <span className="circle big" />
+        <span className="circle med" />
+        <span className="circle small" />
       </div>
     </div>
   )
